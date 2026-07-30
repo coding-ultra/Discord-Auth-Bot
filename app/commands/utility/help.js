4f7dc6f00936058bc538da5261824118b611d7da
coding-ultra <civizsicloudhost@gmail.com>
2026-07-30 20:55:58 +0530

    Update help.js

--- app/commands/utility/help.js
+++ app/commands/utility/help.js
@@ -1,4 +1,8 @@
-// Synora 乂 𝙳evelopment
+// ─────────────────────────────────────────────────────────────────────────────
+//  PRIME DEV  ·  /authlink
+// ─────────────────────────────────────────────────────────────────────────────
+// + Discord: discord.gg/fbGGmgWXy
+// + Community: https://discord.gg/fbGGmgWXy
 
 import {
   SlashCommandBuilder,
